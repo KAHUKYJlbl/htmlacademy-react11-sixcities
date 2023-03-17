1c74187d35370f01f5474029be9d818d417831db
Iurii Pototskii <iuriipototskii2gmail.com>
2023-03-17 18:37:17 +0100

    feat: new comment form data keeps in state

---
+++ project/src/components/new-comment-form/new-comment-form.tsx
@@ -0,0 +1,135 @@
+import { useState } from 'react';
+
+export default function NewCommentForm (): JSX.Element {
+  const [newComment, setNewComment] = useState({
+    rating: '',
+    review: '',
+  });
+
+  const handleRatingChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.checked) {
+      setNewComment({
+        ...newComment,
+        rating: evt.target.value
+      });
+    }
+  };
+
+  const handleRewiewInput = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNewComment({
+      ...newComment,
+      review: evt.target.value
+    });
+  };
+
+  const handleFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    // evt.currentTarget.reset();
+
+    setNewComment({
+      rating: '',
+      review: '',
+    });
+  };
+
+  return (
+    <form
+      className="reviews__form form"
+      action="#"
+      method="post"
+      onSubmit={handleFormSubmit}
+    >
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <div className="reviews__rating-form form__rating">
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="5"
+          id="5-stars"
+          type="radio"
+          checked={newComment.rating === '5'}
+          onChange={handleRatingChange}
+        />
+        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="4"
+          id="4-stars"
+          type="radio"
+          checked={newComment.rating === '4'}
+          onChange={handleRatingChange}
+        />
+        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="3"
+          id="3-stars"
+          type="radio"
+          checked={newComment.rating === '3'}
+          onChange={handleRatingChange}
+        />
+        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="2"
+          id="2-stars"
+          type="radio"
+          checked={newComment.rating === '2'}
+          onChange={handleRatingChange}
+        />
+        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="1"
+          id="1-star"
+          type="radio"
+          checked={newComment.rating === '1'}
+          onChange={handleRatingChange}
+        />
+        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+      </div>
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review" name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={newComment.review}
+        onInput={handleRewiewInput}
+      >
+      </textarea>
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button className="reviews__submit form__submit button" type="submit">Submit</button>
+      </div>
+    </form>
+  );
+}
