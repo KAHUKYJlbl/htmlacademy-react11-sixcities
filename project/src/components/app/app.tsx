8629e8b4f680993edacda8760200c4870575448e
Iurii Pototskii <iuriipototskii2gmail.com>
2023-04-23 18:58:00 +0200

    feat: lazy load

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from '../private-route/private-route';
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 
 import Main from '../../pages/main/main';
 
@@ -24,35 +24,37 @@ export default function App(): JSX.Element {
   }
 
   return (
-    <HistoryRouter history={browserHistory}>
-      <Routes>
-        <Route
-          path={AppRoute.Main}
-          element={
-            <Main />
-          }
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<Login />}
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element={
-            <PrivateRoute>
-              <Favorites />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={AppRoute.Room}
-          element={<Room />}
-        />
-        <Route
-          path="*"
-          element={<NotFound />}
-        />
-      </Routes>
-    </HistoryRouter>
+    <Suspense fallback={<LoadingSpinner spinnerType='page' />}>
+      <HistoryRouter history={browserHistory}>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={
+              <Main />
+            }
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<Login />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute>
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Room}
+            element={<Room />}
+          />
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+        </Routes>
+      </HistoryRouter>
+    </Suspense>
   );
 }
